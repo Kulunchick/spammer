3de9662e642eb63c27e1819d6376c33effdc5369
kulunchick <nikolikrasovski@gmail.com>
2021-03-15 15:20:57 +0200

    Image

--- spammer.py
+++ spammer.py
@@ -1,9 +1,18 @@
 import sys
 import pexpect
 from time import sleep
+import os
+from random import randint
 
-sticker = '/data/data/com.termux/files/home/spammer/blinb.tgs'
-chat = 'windy31chat'
+dir = '/data/data/com.termux/files/home/spammer'
+
+video = os.listdir(f'{dir}/video')
+image = os.listdir(f'{dir}/image')
+sticker = f'{dir}/blinb.tgs'
+
+type_spam = 'video'
+
+chat = 'cubecatinprog'
 
 tg = pexpect.spawn('telegram-cli --json --disable-output')
 
@@ -14,13 +23,23 @@ tg.expect('id')
 tg.sendline(f'channel_join @{chat}')
 print('Join')
 tg.expect('SUCCESS')
-line = f'send_document @{chat} {sticker}'
-tg.sendline(line)
+if type_spam == 'sticker':
+    line = f'send_document @{chat} {sticker}'
+    def spam():
+        tg.sendline(line)
+elif type_spam == 'video':
+    line = f'send_video @{chat} %s'
+    def spam():
+        tg.sendline(line % f'{dir}/video/{video[randint(0, len(video)-1)]}')
+elif type_spam == 'image':
+    line = f'send_photo @{chat} %s'
+    def spam():
+        tg.sendline(line % f'{dir}/image/{video[randint(0, len(video)-1)]}')
 for i in range(10000):
     try:
         tg.expect('SUCCESS', timeout=1)
-        tg.sendline(line)
+        spam()
         print('OK')
         sleep(0.1)
     except:
-        tg.sendline(line)
+        spam()
